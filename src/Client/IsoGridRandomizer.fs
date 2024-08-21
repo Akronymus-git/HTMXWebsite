ea5c49b8f2ed99b3ba78ec71cc730f8fd9be63fa
Akronymus <akronymus@akronymus.net>
2024-08-21 22:31:22 +0200

    Bounds checked iso grid so it doesn't crash the server

--- src/Client/IsoGridRandomizer.fs
+++ src/Client/IsoGridRandomizer.fs
@@ -4,16 +4,25 @@ open System
 open Giraffe.ViewEngine
 
 let IsoGridRandomizer seed xSize ySize =
-    
+    let xs =
+        match xSize with
+        | s when s > 100 -> 100
+        | s when s < 1 -> 1
+        | s -> s
+    let ys =
+        match ySize with
+        | s when s > 100 -> 100
+        | s when s < 1 -> 1
+        | s -> s
     let random = new Random(seed)
-    let randx = random.Next(0,xSize)
-    let randy = random.Next(0,ySize)
+    let randx = random.Next(0,xs)
+    let randy = random.Next(0,ys)
     SharedView.basicPage
         "IsoGridRandomier"
         [
             div [_class "isoGridContainer"; _style $"grid-template-columns: repeat({xSize},1fr);"] [
-                for i in 0..xSize - 1 do
-                    for j in 0..ySize - 1 do
+                for i in 0..xs - 1 do
+                    for j in 0..ys - 1 do
                         match i,j with
                         | x, y when x = randx && y = randy -> div [_class "tile hit"; _style "aspect-ratio:1"] [ Text $"{i},{j}" ] 
                         | _ -> div [_class "tile grass"; _style "aspect-ratio:1"] [ Text $"{i},{j}" ]
