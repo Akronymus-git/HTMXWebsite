debcf86303772d16f3e1887e0a98426fe8ce1831
Akronymus <akronymus@akronymus.net>
2026-01-03 05:13:20 +0100

    Fixed error

--- src/Shared/User.fs
+++ src/Shared/User.fs
@@ -8,11 +8,12 @@ open Giraffe.Core
 
 let WithUser (dbcontext: DBContext.Data) (next:HttpFunc) (ctx:HttpContext) =
     let authcookie = (ctx.Request.Cookies.Item "user-auth")
-    let user = dbcontext.Users.FindUserBySession (Guid.Parse authcookie) |> Async.RunSynchronously
-    match user with
-    | Some u ->
-        ctx.Items.Add ("userName", u.Name)
-        ctx.Items.Add ("userEmail", u.Email)
-    | _ -> ()
+    if authcookie <> null then
+        let user = dbcontext.Users.FindUserBySession (Guid.Parse authcookie) |> Async.RunSynchronously
+        match user with
+        | Some u ->
+            ctx.Items.Add ("userName", u.Name)
+            ctx.Items.Add ("userEmail", u.Email)
+        | _ -> ()
     next ctx
 
